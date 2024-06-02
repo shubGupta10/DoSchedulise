8a20470fa9d40857ac19661eca5110f46b4fec9e
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-06-02 20:12:09 +0530

    Fixing for Production

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -10,7 +10,7 @@ import closeIcon from '../../ChatLoginImage/closeIcon.png';
 
 let socket;
 
-const ENDPOINT= `${import.meta.env.BAKCEND_URL}`;
+const ENDPOINT= `${import.meta.env.VITE_BACKEND_URL}`;
 
 const Chat = () => {
   const [id , setId] = useState("");
